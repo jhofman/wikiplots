16b41fb98ee7663b28694739c3dbb97a048bce01
Jake Hofman <jhofman+github@gmail.com>
2011-06-03 09:46:09 -0400

    numbers have priority over dates

--- htdocs/scripts/parse.js
+++ htdocs/scripts/parse.js
@@ -34,8 +34,8 @@ function select_table(table) {
     $(table).before('<div id="wikiplot" style="width:400px;height:300px"></div>');
 
     //wikiplot(df, {x: df.col_names[2], y: df.col_names[4]}, $("#wikiplot"));
-    wikiplot(df, {x: df.col_names[1], y: df.col_names[4]}, $("#wikiplot"));
-    //wikiplot(df, {x: df.col_names[3], y: df.col_names[4]}, $("#wikiplot"));
+    //wikiplot(df, {x: df.col_names[1], y: df.col_names[4]}, $("#wikiplot"));
+    wikiplot(df, {x: df.col_names[3], y: df.col_names[4]}, $("#wikiplot"));
 
 }
 
@@ -121,7 +121,8 @@ function array_type(v, types) {
 }
 
 function col_type(col) {
-    var type = array_type(col, ['datetime','percentage','currency','number']);
+    //var type = array_type(col, ['datetime','percentage','currency','number']); // date has priority over number
+    var type = array_type(col, ['number','datetime','percentage','currency']); // number has priority over date
 
     if (!type)
 	return 'text';
